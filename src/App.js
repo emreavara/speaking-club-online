92a2f4751bc4c4f9cae2c2ad6da10ab9fddd822f
Avara, Emre <emre_avara@hotmail.com>
2022-10-11 00:26:17 +0200

    Add Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import Main from "./pages/Main";
+
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <Main/>
   );
 }
 
---
+++ src/pages/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navi from '../components/Navi'
+
+function Main() {
+  return (
+    <>
+    <Navi/>
+    </>
+  )
+}
+
+export default Main
