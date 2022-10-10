8c204e78f1bbcb21d28ff069b04b49fc72fb731e
Avara, Emre <emre_avara@hotmail.com>
2022-10-11 01:16:12 +0200

    Create Meeting Card

---
+++ src/components/MeetingCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import Button from "@mui/material/Button";
+import Box from '@mui/material/Box';
+function MeetingCard({ data }) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        title={data.title}
+        subheader={
+          <>
+            <AccessTimeIcon />
+            {data.time}
+            {data.date}
+          </>
+        }
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image={data.imgSource}
+        alt="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {data.description}
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: "center",alignItems: 'center', marginY: 3}} >
+          <Button size="large" variant="contained">Join</Button>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default MeetingCard;
