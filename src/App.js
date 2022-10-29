95751267b49d2ecb171d41987ba1c660a4463c43
Avara, Emre <emre_avara@hotmail.com>
2022-10-29 10:36:03 +0200

    Add info card for pages

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Main from "./pages/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MeetingCard from "./components/MeetingCard";
 import MeetingDetails from "./pages/MeetingDetails";
-
+import meetings from "./data/dummy";
 function App() {
   const germanA2B1 = "German A2 B1 ";
+  console.log(meetings[2])
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}/>
-        <Route path="/german-a2-b1" element={<MeetingDetails meetingInfo={germanA2B1} />} />
+        <Route path="/german-a2-b1" element={<MeetingDetails meetingInfo={meetings[2]} />} />
+        <Route path="/english-a2-b1" element={<MeetingDetails meetingInfo={meetings[2]} />} />
         <Route path="*" element={<Main />}/>
       </Routes>
     </BrowserRouter>
--- src/components/MeetingInfo.js
+++ src/components/MeetingInfo.js
@@ -1,11 +1,60 @@
-import React from 'react'
-
-function MeetingInfo({meeting}) {
+import React from "react";
+import {
+  Grid,
+  Box,
+  Stack,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  List,
+  ListItem,
+  ListItemText,
+  Divider
+} from "@mui/material";
+import { height } from "@mui/system";
+function MeetingInfo({ meeting }) {
   return (
-    <div>
-      {meeting}
-    </div>
-  )
+    <Grid container>
+      <Grid item sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            padding: "5px",
+            width: "100%",
+            height: "250px",
+          }}
+        >
+          <img
+            src="https://media.baamboozle.com/uploads/images/368954/1623241895_147909.jpeg"
+            alt="Lang"
+          />
+        </Box>
+      </Grid>
+      <Grid item sx={{ width: "100%", padding: "5px" }}>
+        <Stack spacing={3} direction="row">
+          <Box xs={4}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardHeader title="Target Words" />
+              <CardContent>
+                <List>
+                  <ListItemText>Name</ListItemText>
+                </List>
+              </CardContent>
+            </Card>
+          </Box>
+
+          <Box xs={3} sm={3} sx={{ backgroundColor: "white", height: "300px" }}>
+            <Typography>Text</Typography>
+          </Box>
+          <Box xs={3} sm={3} sx={{ backgroundColor: "white", height: "300px" }}>
+            <Typography>Text</Typography>
+          </Box>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
 
-export default MeetingInfo
+export default MeetingInfo;
